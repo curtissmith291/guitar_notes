91a740c618edf64835833c90744f76e18a3070d1
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-07-13 22:28:28 -0500

    added code that returns the note based on fret num + string

---
+++ notes.py
@@ -0,0 +1,49 @@
+notes_flats = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
+notes_sharps = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+all_notes = ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'A#', 'C#', 'D#', 'F#', 'G#']
+
+string_names = {1: 'e', 2: 'B', 3: 'G', 4: 'G', 5: 'A', 6: 'E'}
+string_values = {'E': 6, 'A': 5, 'D': 4, 'G': 3, 'B': 2, 'e': 1}
+
+# chrom = list(range(1,13))
+# flats = dict(zip(chrom, notes_flats))
+# print(flats)
+
+note_values_flats = {'Ab': 1, 'A': 2, 'Bb': 3, 'B': 4, 'C': 5, 'Db': 6, 'D': 7, 'Eb': 8, 'E': 9, 
+    'F': 10, 'Gb': 11, 'G': 12}
+
+value_notes_sharps = {1: 'A', 2: 'A#', 3: 'B', 4: 'C', 5: 'C#', 6: 'D', 7: 'D#', 8: 'E', 9: 'F', 
+    10: 'F#', 11: 'G', 12: 'G#'}
+
+value_notes_flats = {1: 'A', 2: 'Bb', 3: 'B', 4: 'C', 5: 'Db', 6: 'D', 7: 'Eb', 8: 'E', 9: 'F', 
+    10: 'Gb', 11: 'G', 12: 'Ab'}
+
+
+# User Inputs the string, each string has a note value corresponding to the 12 notes
+string_chrom_value = {'E': 8, 'A': 1, 'D': 6, 'G': 11, 'B': 3, 'e': 8}
+
+string_names = ['E', 'A', 'D', 'G', 'B', 'e']
+
+while True:
+    string_input = input("Enter String Name (E, A, D, G, B, e) ")
+    if string_input not in string_names:
+        print("Please enter a string name")
+        continue
+    else:
+        break
+
+fret_input = int(input("Enter fret number "))
+
+def note_value(string, fret):
+    note_num = string_chrom_value[string] + fret
+    # checks if number is >12, if so, subtracts 12 until it's 12 or lower
+    while True:
+        if note_num > 12:
+            note_num = note_num - 12
+            continue
+        else:
+            break
+    note = value_notes_sharps[note_num]
+    return note
+
+print(note_value(string_input, fret_input))
