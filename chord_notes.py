c274ef58347f6e2a99ef5c23e76bb6c6fe1f96c2
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-07-13 23:46:23 -0500

    fixed/added dictionaries; added ficntion to return the note in a major chord

---
+++ chord_notes.py
@@ -0,0 +1,33 @@
+note_values_flats = {'A': 1, 'Bb': 2, 'B': 3, 'C': 4, 'Db': 5, 'D': 6, 'Eb': 7, 'E': 8, 'F': 9, 
+    'Gb': 10, 'G': 11, 'Ab': 12}
+
+note_values_sharps = {'A': 1, 'A#': 2, 'B': 3, 'C': 4, 'C#': 5, 'D': 6, 'D#': 7, 'E': 8, 'F': 9, 
+    'F#': 10, 'G': 11, 'G#': 12}
+
+note_values = {'A': 1, 'A#/Bb': 2, 'B': 3, 'C': 4, 'C#/Db': 5, 'D': 6, 'D#/Eb': 7, 'E': 8, 'F': 9, 
+    'F#/Gb': 10, 'G': 11, 'G#/Ab': 12}
+
+value_notes_sharps = {1: 'A', 2: 'A#', 3: 'B', 4: 'C', 5: 'C#', 6: 'D', 7: 'D#', 8: 'E', 9: 'F', 
+    10: 'F#', 11: 'G', 12: 'G#'}
+
+value_notes = {1: 'A', 2: 'A#/Bb', 3: 'B', 4: 'C', 5: 'C#/Db', 6: 'D', 7: 'D#/Eb', 8: 'E', 9: 'F', 
+    10: 'F#/Gb', 11: 'G', 12: 'G#/Ab'}
+
+value_notes_flats = {1: 'A', 2: 'Bb', 3: 'B', 4: 'C', 5: 'Db', 6: 'D', 7: 'Eb', 8: 'E', 9: 'F', 
+    10: 'Gb', 11: 'G', 12: 'Ab'}
+
+input = "G"
+
+def chord_notes(note):
+    value = note_values[note]
+    third_num = value + 4
+    if third_num > 12:
+        third_num -= 12
+    third = value_notes[third_num]
+    fifth_num = value + 7
+    if fifth_num > 12:
+        fifth_num -= 12
+    fifth = value_notes[fifth_num]
+    return note, third, fifth
+
+print(chord_notes(input))
--- notes.py
+++ notes.py
@@ -1,15 +1,24 @@
 notes_flats = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
 notes_sharps = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-all_notes = ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'A#', 'C#', 'D#', 'F#', 'G#']
 
 string_names = {1: 'e', 2: 'B', 3: 'G', 4: 'G', 5: 'A', 6: 'E'}
 string_values = {'E': 6, 'A': 5, 'D': 4, 'G': 3, 'B': 2, 'e': 1}
 
-note_values_flats = {'Ab': 1, 'A': 2, 'Bb': 3, 'B': 4, 'C': 5, 'Db': 6, 'D': 7, 'Eb': 8, 'E': 9, 
-    'F': 10, 'Gb': 11, 'G': 12}
+# These threee are inconsistant, FIX!!, All need to start at "A"
+note_values_flats = {'A': 1, 'Bb': 2, 'B': 3, 'C': 4, 'Db': 5, 'D': 6, 'Eb': 7, 'E': 8, 'F': 9, 
+    'Gb': 10, 'G': 11, 'Ab': 12}
+
+note_values_sharps = {'A': 1, 'A#': 2, 'B': 3, 'C': 4, 'C#': 5, 'D': 6, 'D#': 7, 'E': 8, 'F': 9, 
+    'F#': 10, 'G': 11, 'G#': 12}
+
+note_values = {'A': 1, 'A#/Bb': 2, 'B': 3, 'C': 4, 'C#/Db': 5, 'D': 6, 'D#/Eb': 7, 'E': 8, 'F': 9, 
+    'F#/Gb': 10, 'G': 11, 'G#/Ab': 12}
 
 value_notes_sharps = {1: 'A', 2: 'A#', 3: 'B', 4: 'C', 5: 'C#', 6: 'D', 7: 'D#', 8: 'E', 9: 'F', 
     10: 'F#', 11: 'G', 12: 'G#'}
+
+value_notes = {1: 'A', 2: 'A#/Bb', 3: 'B', 4: 'C', 5: 'C#/Db', 6: 'D', 7: 'D#/Eb', 8: 'E', 9: 'F', 
+    10: 'F#/Gb', 11: 'G', 12: 'G#/Ab'}
 
 value_notes_flats = {1: 'A', 2: 'Bb', 3: 'B', 4: 'C', 5: 'Db', 6: 'D', 7: 'Eb', 8: 'E', 9: 'F', 
     10: 'Gb', 11: 'G', 12: 'Ab'}
@@ -32,13 +41,9 @@ while True:
 # Any fret number is technically valid as the pattern is infinately repeating
 while True:
     try:
+        # If input if not an integer, an error occurs and the "except" path is taken
         fret_input = int(input("Enter fret number "))
         break
-        # if type(fret_input) != int:
-        #     print("Please enter a valid fret number")
-        #     continue
-        # else:
-        #     break
     except:
         print("Please enter a valid fret number")
         continue
