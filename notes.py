50383542b71630d193814f0b58d9fc0f4e92ade4
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-07-13 23:05:27 -0500

    added checks for user inputs

--- notes.py
+++ notes.py
@@ -5,10 +5,6 @@ all_notes = ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'A
 string_names = {1: 'e', 2: 'B', 3: 'G', 4: 'G', 5: 'A', 6: 'E'}
 string_values = {'E': 6, 'A': 5, 'D': 4, 'G': 3, 'B': 2, 'e': 1}
 
-# chrom = list(range(1,13))
-# flats = dict(zip(chrom, notes_flats))
-# print(flats)
-
 note_values_flats = {'Ab': 1, 'A': 2, 'Bb': 3, 'B': 4, 'C': 5, 'Db': 6, 'D': 7, 'Eb': 8, 'E': 9, 
     'F': 10, 'Gb': 11, 'G': 12}
 
@@ -18,12 +14,12 @@ value_notes_sharps = {1: 'A', 2: 'A#', 3: 'B', 4: 'C', 5: 'C#', 6: 'D', 7: 'D#',
 value_notes_flats = {1: 'A', 2: 'Bb', 3: 'B', 4: 'C', 5: 'Db', 6: 'D', 7: 'Eb', 8: 'E', 9: 'F', 
     10: 'Gb', 11: 'G', 12: 'Ab'}
 
-
-# User Inputs the string, each string has a note value corresponding to the 12 notes
 string_chrom_value = {'E': 8, 'A': 1, 'D': 6, 'G': 11, 'B': 3, 'e': 8}
 
 string_names = ['E', 'A', 'D', 'G', 'B', 'e']
 
+
+# User inputs string name, checks if valid input
 while True:
     string_input = input("Enter String Name (E, A, D, G, B, e) ")
     if string_input not in string_names:
@@ -32,7 +28,21 @@ while True:
     else:
         break
 
-fret_input = int(input("Enter fret number "))
+# User inputs fret number
+# Any fret number is technically valid as the pattern is infinately repeating
+while True:
+    try:
+        fret_input = int(input("Enter fret number "))
+        break
+        # if type(fret_input) != int:
+        #     print("Please enter a valid fret number")
+        #     continue
+        # else:
+        #     break
+    except:
+        print("Please enter a valid fret number")
+        continue
+  
 
 def note_value(string, fret):
     note_num = string_chrom_value[string] + fret
